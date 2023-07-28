6f79ef2a883b53dfaf68af87421d2fc063ceb6ab
Mohit Jain <jainmohit2001@gmail.com>
2023-07-28 18:16:31 +0530

    added comments and JSDOC

--- src/6/random_sort.ts
+++ src/6/random_sort.ts
@@ -28,6 +28,13 @@ class StringHash implements IStringHash {
   }
 }
 
+/**
+ * Computes the Hash value of a given string using
+ * Polynomial Rolling Hash Function.
+ *
+ * @param {string} str
+ * @returns {number}
+ */
 function computeHash(str: string): number {
   const prime = 31;
   const mod = 10e9 + 9;
@@ -43,6 +50,14 @@ function computeHash(str: string): number {
   return hashValue;
 }
 
+/**
+ * Given an array of number, this function randomly shuffles it's content and
+ * returns the new shuffled array.
+ * @date 7/28/2023 - 6:12:23 PM
+ *
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
 function shuffleArray(arr: number[]): number[] {
   for (let i = 0; i < arr.length; i++) {
     // Generate random index to swap from
@@ -56,30 +71,37 @@ function shuffleArray(arr: number[]): number[] {
 }
 
 function randomSort(arr: string[]): string[] {
+  // Storing hash value and corresponding information
   const map = new Map<number, IStringHash>();
 
   for (let i = 0; i < arr.length; i++) {
     const hash = computeHash(arr[i]);
+    // If hash already exists
     if (!map.has(hash)) {
       map.set(hash, new StringHash(arr[i], 1, hash));
-    } else {
+    }
+    // Otherwise increment the count
+    else {
       map.get(hash)!.incrementCount();
     }
   }
 
-  const hashArr = shuffleArray([...map.keys()]);
+  const hashArr = shuffleArray([...map.keys()]); // Shuffle the hash values
   const newArray: string[] = [];
 
   for (let i = 0; i < hashArr.length; i++) {
     const hash = hashArr[i];
     if (map.has(hash)) {
+      // Get the element and push the string `count` times
       const elem = map.get(hash)!;
       let count = elem.count;
       const str = elem.str;
+
       while (count > 0) {
         newArray.push(str);
         count--;
       }
+
       map.delete(hash);
     } else {
       // Should never happen
