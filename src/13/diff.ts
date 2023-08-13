6eda34cf8bc159fc86d8e539d4ecc54296e151ab
Mohit Jain <jainmohit2001@gmail.com>
2023-08-13 14:00:58 +0530

    Completed diff toll with test cases

--- src/13/diff.ts
+++ src/13/diff.ts
@@ -1,4 +1,15 @@
-export function lcs(s1: string, s2: string): string {
+import fs from 'fs';
+
+/**
+ * Finds the longest common subsequence for given string s1 and s2.
+ * Returns the inserted and deleted characters along with the lcs.
+ *
+ * @export
+ * @param {string} s1
+ * @param {string} s2
+ * @returns {string[]} - [lcs, inserted characters, deleted characters]
+ */
+export function lcs(s1: string, s2: string): string[] {
   const m = s1.length;
   const n = s2.length;
 
@@ -35,24 +46,154 @@ export function lcs(s1: string, s2: string): string {
   let str = '';
   let i = m,
     j = n;
+  let inserted = '',
+    deleted = '';
 
   while (i > 0 && j > 0) {
     // Include the character if both the characters are same
     if (s1[i - 1] == s2[j - 1]) {
-      str += s1[i - 1];
+      str = s1[i - 1] + str;
       i--;
       j--;
     }
     // Else move in the direction with greater value
     else {
       if (lcsTable[i][j - 1] > lcsTable[i - 1][j]) {
+        inserted = s2[j - 1] + inserted;
         j--;
       } else {
+        deleted = s1[i - 1] + deleted;
         i--;
       }
     }
   }
 
-  // We need to reverse the string
-  return str.split('').reverse().join('');
+  while (i > 0) {
+    deleted = s1[i - 1] + deleted;
+    i--;
+  }
+
+  while (j > 0) {
+    inserted = s2[j - 1] + inserted;
+    j--;
+  }
+
+  return [str, inserted, deleted];
+}
+
+export function lcsArray(arr1: string[], arr2: string[]): string[][] {
+  const m = arr1.length;
+  const n = arr2.length;
+
+  // Initialize LCS Table
+  const lcsTable = new Array<Array<number>>(m + 1);
+  for (let i = 0; i <= m; i++) {
+    lcsTable[i] = new Array<number>(n + 1);
+  }
+
+  // Fill First column with zeroes
+  for (let i = 0; i <= m; i++) {
+    lcsTable[i][0] = 0;
+  }
+
+  // Fill first row with zeroes
+  for (let j = 0; j <= n; j++) {
+    lcsTable[0][j] = 0;
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      // If both the strings match
+      if (arr1[i - 1] === arr2[j - 1]) {
+        lcsTable[i][j] = lcsTable[i - 1][j - 1] + 1;
+      }
+      // Choose the max of the neighbors
+      else {
+        lcsTable[i][j] = Math.max(lcsTable[i - 1][j], lcsTable[i][j - 1]);
+      }
+    }
+  }
+
+  // Find the LCS from the Table
+  const str: string[] = [];
+  let i = m,
+    j = n;
+  let insertions = new Array<{ index: number; str: string }>();
+  let deletions = new Array<{ index: number; str: string }>();
+
+  while (i > 0 && j > 0) {
+    // Include the string if both the strings are same
+    if (arr1[i - 1] === arr2[j - 1]) {
+      str.push(arr1[i - 1]);
+      i--;
+      j--;
+    }
+    // Else move in the direction with greater value
+    else {
+      if (lcsTable[i][j - 1] > lcsTable[i - 1][j]) {
+        insertions.push({ index: j - 1, str: '+ ' + arr2[j - 1] });
+        j--;
+      } else {
+        deletions.push({ index: i - 1, str: '- ' + arr1[i - 1] });
+        i--;
+      }
+    }
+  }
+
+  while (j > 0) {
+    insertions.push({ index: j - 1, str: '+ ' + arr2[j - 1] });
+    j--;
+  }
+
+  while (i > 0) {
+    deletions.push({ index: i - 1, str: '- ' + arr1[i - 1] });
+    i--;
+  }
+
+  const differences: string[] = [];
+
+  insertions = insertions.reverse();
+  deletions = deletions.reverse();
+
+  let x = 0,
+    y = 0;
+  const length1 = insertions.length,
+    length2 = deletions.length;
+
+  while (x < length1 && y < length2) {
+    if (deletions[y].index <= insertions[x].index) {
+      differences.push(deletions[y].str);
+      y++;
+    } else {
+      differences.push(insertions[x].str);
+      x++;
+    }
+  }
+
+  while (x < length1) {
+    differences.push(insertions[x].str);
+    x++;
+  }
+
+  while (y < length2) {
+    differences.push(deletions[y].str);
+    y++;
+  }
+
+  // We need to reverse the array of strings
+  return [str.reverse(), differences];
+}
+
+export function diffBetweenFiles(path1: string, path2: string) {
+  const arr1 = fs
+    .readFileSync(path1)
+    .toString()
+    .split(/\r\n|\n/);
+
+  const arr2 = fs
+    .readFileSync(path2)
+    .toString()
+    .split(/\r\n|\n/);
+
+  return lcsArray(arr1, arr2);
 }
--- tests/13/diff.test.ts
+++ tests/13/diff.test.ts
@@ -1,23 +1,87 @@
-import { lcs } from '../../src/13/diff';
+import { lcs, lcsArray } from '../../src/13/diff';
 
-describe('Testing LCS Function', () => {
-  const data = [
-    ['ABCDEFG', 'ABCDEFG', 'ABCDEFG'],
-    ['ABCDEFG', '', ''],
-    ['ABC', 'XYZ', ''],
-    ['AABCXY', 'XYZ', 'XY'],
-    ['', '', ''],
-    ['ABCD', 'AC', 'AC']
+describe('Testing lcs Function', () => {
+  // [s1, s2]
+  const inputs: string[][] = [
+    ['ABCDEFG', 'ABCDEFG'],
+    ['ABCDEFG', ''],
+    ['ABC', 'XYZ'],
+    ['AABCXY', 'XYZ'],
+    ['', ''],
+    ['ABCD', 'AC']
   ];
 
-  data.forEach((value) => {
-    const s1 = value[0];
-    const s2 = value[1];
-    const expectedOutput = value[2];
+  // [lcs, inserted, deleted]
+  const outputs: string[][] = [
+    ['ABCDEFG', '', ''],
+    ['', '', 'ABCDEFG'],
+    ['', 'XYZ', 'ABC'],
+    ['XY', 'Z', 'AABC'],
+    ['', '', ''],
+    ['AC', '', 'BD']
+  ];
+
+  inputs.forEach((input, index) => {
+    const s1 = input[0];
+    const s2 = input[1];
+    const expectedOutput = outputs[index];
 
     test(`Testing s1:${s1}, s2:${s2}`, () => {
       const output = lcs(s1, s2);
-      expect(output).toBe(expectedOutput);
+      expect(output).toStrictEqual(expectedOutput);
+    });
+  });
+});
+
+describe('Testing lcsArray function', () => {
+  const inputs = [
+    [
+      ['This is a test which contains:', 'this is the lcs'],
+      ['this is the lcs', "we're testing"]
+    ],
+    [
+      [
+        'Coding Challenges helps you become a better software engineer through that build real applications.',
+        'I share a weekly coding challenge aimed at helping software engineers level up their skills through deliberate practice.',
+        "I've used or am using these coding challenges as exercise to learn a new programming language or technology.",
+        'Each challenge will have you writing a full application or tool. Most of which will be based on real world tools and utilities.'
+      ],
+      [
+        'Helping you become a better software engineer through coding challenges that build real applications.',
+        'I share a weekly coding challenge aimed at helping software engineers level up their skills through deliberate practice.',
+        "These are challenges that I've used or am using as exercises to learn a new programming language or technology.",
+        'Each challenge will have you writing a full application or tool. Most of which will be based on real world tools and utilities.'
+      ]
+    ]
+  ];
+
+  const outputs = [
+    [
+      ['this is the lcs'],
+      ['- This is a test which contains:', "+ we're testing"]
+    ],
+    [
+      [
+        'I share a weekly coding challenge aimed at helping software engineers level up their skills through deliberate practice.',
+        'Each challenge will have you writing a full application or tool. Most of which will be based on real world tools and utilities.'
+      ],
+      [
+        '- Coding Challenges helps you become a better software engineer through that build real applications.',
+        '+ Helping you become a better software engineer through coding challenges that build real applications.',
+        "- I've used or am using these coding challenges as exercise to learn a new programming language or technology.",
+        "+ These are challenges that I've used or am using as exercises to learn a new programming language or technology."
+      ]
+    ]
+  ];
+
+  inputs.forEach((input, index) => {
+    const arr1 = input[0];
+    const arr2 = input[1];
+    const expectedOutput = outputs[index];
+
+    test(`Testing index:${index}`, () => {
+      const output = lcsArray(arr1, arr2);
+      expect(output).toStrictEqual(expectedOutput);
     });
   });
 });
