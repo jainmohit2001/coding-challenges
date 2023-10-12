c66690a17e16792549ec5df1cd6f1b78b095dfcd
jainmohit2001 <jainmohit2001@gmail.com>
2023-10-12 21:59:20 +0530

    Added command for setting system time.

--- src/28/client.ts
+++ src/28/client.ts
@@ -1,11 +1,20 @@
 import UDP from 'dgram';
-import { NtpPacket, createNtpPacket, parseNtpPacket } from './ntp-packet';
+import {
+  NtpPacket,
+  calculateOffset,
+  calculateRoundTripDelay,
+  createNtpPacket,
+  parseNtpPacket
+} from './ntp-packet';
 import { PORT } from './constants';
+import { exec } from 'child_process';
+import os from 'os';
 
 export class NtpClient {
   server: string;
   timeout = 10000;
   client: UDP.Socket;
+  replyPacket?: NtpPacket;
 
   constructor(server?: string) {
     this.server = server ?? '0.pool.ntp.org';
@@ -39,11 +48,48 @@ export class NtpClient {
 
         this.client.once('message', (msg) => {
           const packet = parseNtpPacket(msg);
+          this.replyPacket = packet;
           clearTimeout(timeout);
           this.client.close();
           res(packet);
         });
       });
+    });
+  }
+
+  offset(): number {
+    if (!this.replyPacket) {
+      throw new Error('Invalid replyPacket');
+    }
+    return calculateOffset(this.replyPacket);
+  }
+
+  rtt(): number {
+    if (!this.replyPacket) {
+      throw new Error('Invalid replyPacket');
+    }
+    return calculateRoundTripDelay(this.replyPacket);
+  }
+
+  setSystemTime(date: Date): Promise<void> {
+    return new Promise<void>((res, rej) => {
+      const platform = os.platform();
+      if (platform === 'linux') {
+        exec(
+          `/bin/date --set="${date.toISOString()}"`,
+          (err, stdout, stderr) => {
+            if (err) {
+              rej(err);
+            } else if (stderr) {
+              rej(stderr);
+            } else {
+              res();
+            }
+          }
+        );
+      } else {
+        rej(`Setting system time for ${platform} is currently not supported`);
+      }
     });
   }
 }
--- src/28/index.ts
+++ src/28/index.ts
@@ -1,15 +1,29 @@
 import { NtpClient } from './client';
 
-import { calculateOffset, calculateRoundTripDelay } from './ntp-packet';
-
 const client = new NtpClient();
 
 client
   .getTime()
   .then((msg) => {
-    console.log(msg);
-    console.log(`Offset (theta) ${calculateOffset(msg)}ms`);
-    console.log(`RTT (delta) ${calculateRoundTripDelay(msg)}ms`);
+    console.log('NTP Packet', msg);
+
+    const offset = client.offset();
+    console.log(`Offset (theta) ${offset}ms`);
+
+    const rtt = client.rtt();
+    console.log(`RTT (delta) ${rtt}ms`);
+
+    const now = new Date();
+    now.setTime(now.getTime() + offset);
+
+    client
+      .setSystemTime(now)
+      .then(() => {
+        console.log('Successfully set system time');
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   })
   .catch((err) => {
     console.error(err);
